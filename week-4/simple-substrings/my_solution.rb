79f919db40d8096481270918be1f5d9977b8d326
Lauren Markzon <lmarkzon@gmail.com>
2015-09-09 16:40:20 -0500

    Substrings

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,12 @@
+def welcome (address)
+	if address.include?("CA")
+		p 'Welcome to California'
+	else
+		p 'You should move to California'
+	end
+end
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
