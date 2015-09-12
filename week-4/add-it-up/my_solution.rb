56084ca63892b44c09abba2a9d6857d659e1f04b
Lauren Markzon <lmarkzon@gmail.com>
2015-09-12 18:53:55 -0500

    add it up sucks ass

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,49 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [by myself, ].
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input: array of [numbers]
+# Output: sum of all the numbers (sum)
+# Steps to solve the problem.
+	# 1) take input and define
+	# 2) take each number within the array and add each together
+	# 3) return output
+
+
+# 1. total initial solution
+def total(array)
+	array = [0]
+		
+		array.each do |i|
+			total += i
+		
+	end
+end
+
+
+#array.each do |i|
+ # 		sum += [i]
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+
+
+
+# 6. sentence_maker refactored solution
