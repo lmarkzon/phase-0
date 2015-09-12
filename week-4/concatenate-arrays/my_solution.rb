fe9ad1c1e5992f57d4bfad58c679782aeafa35e0
Lauren Markzon <lmarkzon@gmail.com>
2015-09-12 10:39:11 -0500

    Concatenate arrays

--- week-4/concatenate-arrays/my_solution.rb
+++ week-4/concatenate-arrays/my_solution.rb
@@ -2,10 +2,15 @@
 
 # I worked on this challenge [by myself,: ].
 
+#My initial solution that only had 1 failed example let me to my answer below.
+#def array_concat(array_1, array_2)
+#array = (array_1 + array_2).sort
+  #p array
+#end
 
 # Your Solution Below
 
 def array_concat(array_1, array_2)
-  array = (array_1 + array_2).sort
-  p array
+  return array_1 + array_2
 end
+
