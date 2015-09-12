7cea17835808479701971ebe9978614b8a0a0ed1
Lauren Markzon <lmarkzon@gmail.com>
2015-09-12 10:39:55 -0500

    Concatenate

--- week-4/concatenate-arrays/my_solution.rb
+++ week-4/concatenate-arrays/my_solution.rb
@@ -2,7 +2,7 @@
 
 # I worked on this challenge [by myself,: ].
 
-#My initial solution that only had 1 failed example let me to my answer below.
+#My initial solution that only had 1 failed example led me to my answer below.
 #def array_concat(array_1, array_2)
 #array = (array_1 + array_2).sort
   #p array
