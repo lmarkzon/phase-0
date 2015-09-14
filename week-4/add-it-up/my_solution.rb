aff9266874dc6a554ac1c61372518d81ae18ef0d
Lauren Markzon <lmarkzon@gmail.com>
2015-09-14 11:59:35 -0500

    sentence maker

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -15,26 +15,34 @@
 
 # 1. total initial solution
 
-def total(array)
-	total = 0
-		array.each { |i| total += i }
-	return total
-end
+#def total(array)
+	#total = 0
+		#array.each { |i| total += i }
+	#return total
+#end
 
 
 # 3. total refactored solution
-
+#def total(array)[0]
+# 	array.each { |i| total += i }
+# end
 
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
-# Steps to solve the problem.
+# Input: strings
+# Output: each object in a sentence
+# Steps to solve the problem: define, join, and return output
 
 
 # 5. sentence_maker initial solution
-
-
+#def sentance_maker(array)
+	#array.join
+	#return sentence_maker
+#end
 
 # 6. sentence_maker refactored solution
+def sentence_maker(array)
+    array.join(" ").capitalize + "."
+end
+
