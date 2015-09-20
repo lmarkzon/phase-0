32ae62d1b6fb8a5807af12ebb6151c4be15a5383
Lauren Markzon <lmarkzon@gmail.com>
2015-09-19 21:17:27 -0500

    Update 5.5

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,6 +1,6 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [] hours on this challenge.
+# I spent [4] hours on this challenge.
 
 # Complete each step below according to the challenge 
 #directions and include it in this file. Also make sure 
@@ -8,29 +8,35 @@
 
 # 0. Pseudocode
 
-# What is the input: a positive integer
+# What is the input: a positive integer without commas
 # - convert positive integer to a string
-# - IF string has less than or equal to 3 characters
-	# - RETURN string
-# - ELSE
-	# - reverse string (so it counts from the end of the integer
-		# every 3 characters)
-	# - iterate over each character
-	# - split and insert comma every 3 characters
-	# - stop when less than 3 characters remaining in the string
-# What is the output: string of integers separated by commas
-
-
+# - reverse string (so it counts from the end of the integer)
+# - iterate each character into array
+# - index? increment each time loop goes
+# - empty array of output?
+# - count every 3 numbers
+# -  add character and comma
+# - convert array to final string
+# - reverse output string?
+# What is the output: string of integers separated by commas correctly
 
 # 1. Initial Solution
-def separate_comma(integer)
-	output_num = integer.to_s
-	if output_num.length <= 3
-		return output_num
-	else output_num.reverse.split(" ").each { |character}
-    end
-end
 
+def separate_comma(integer)
+  index = 1
+  output_string = ("")
+  integer.to_s.reverse.each_char do|character|
+    if index <= 2
+      index = index + 1
+      output_string = output_string + character
+    else
+      index = 1
+      output_string = output_string + character + (",")
+    end
+  end
+  output_string.chomp!(",")
+  return output_string.reverse
+end
 
 
 # 2. Refactored Solution
@@ -39,3 +45,22 @@ end
 
 
 # 3. Reflection
+
+	
+# - What was your process for breaking the problem down? What different approaches did you consider?
+# I think I changed my original psuedocode about 5 times, I kept changing my mind on how I wanted to
+# approach the solution, trying to predict what was going to happen. The first 2 hours I was 
+# convinced I needed to split the array, then after looking in the ruby docs forever I found the
+# each_char 
+
+# - Was your pseudocode effective in helping you build a successful initial solution?
+# For the most part it helped me get started, 
+
+# - What Ruby method(s) did you use when refactoring your solution? What difficulties did you have 
+# implementing it/them? Did it/they significantly change the way your code works? If so, how?
+# For the longest time I got down to 2 errors and could not figure out how to fix it. 
+
+# - How did you initially iterate through the data structure?
+
+
+# - Do you feel your refactored solution is more readable than your initial solution? Why?
