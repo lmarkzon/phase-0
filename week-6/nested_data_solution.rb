77a8b87c56a6a3979eb7dd32d754452a1c94709c
Lauren Markzon <lmarkzon@gmail.com>
2015-09-22 18:22:02 -0500

    6.5

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -1,12 +1,12 @@
-# RELEASE 2: NESTED STRUCTURE GOLF
-# Hole 1
-# Target element: "FORE"
+# # RELEASE 2: NESTED STRUCTURE GOLF
+# # Hole 1
+# # Target element: "FORE"
 
-array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
+# array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
-# attempts:
-# ============================================================
-
+# # attempts:
+# # ============================================================
+# p array[1][1][2][0]
 
 
 # ============================================================
@@ -14,11 +14,11 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 # Hole 2
 # Target element: "congrats!"
 
-hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
+# hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 
-# attempts:
-# ============================================================
-
+# # attempts:
+# # ============================================================
+# p hash[:outer][:inner]["almost"][3]
 
 
 # ============================================================
@@ -27,11 +27,11 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 # Hole 3
 # Target element: "finished"
 
-nested_data = {array: ["array", {hash: "finished"}]}
+# nested_data = {array: ["array", {hash: "finished"}]}
 
-# attempts:
-# ============================================================
-
+# # attempts:
+# # ============================================================
+# p nested_data[:array][1][:hash]
 
 
 # ============================================================
@@ -40,8 +40,25 @@ nested_data = {array: ["array", {hash: "finished"}]}
 
 number_array = [5, [10, 15], [20,25,30], 35]
 
-
+number_array.map! do |element|
+	if element.kind_of?(Array)
+		element.each { |integer| p integer + 5}
+	else p element + 5
+	end
+end
 
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+startup_names.map! do |element|
+	if element.kind_of?(Array)
+		element.each { |element2| 
+			if element2.kind_of?(Array)
+				element2.each { |string| p string + "ly"}
+			else p element2 + "ly"
+			end
+		}
+	else p element + "ly"
+	end
+end
