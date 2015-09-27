f1b049f0a0acec258e2d0c7c02cfcb8ea839a7fb
Lauren Markzon <lmarkzon@gmail.com>
2015-09-27 15:55:15 -0500

    6.7 - reflection not done

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -2,63 +2,166 @@
 
 
 # I worked on this challenge [with: Jamie].
-# I spent [#] hours on this challenge.
+# I spent [6] hours on this challenge (at least!)
 
 # Pseudocode
 
-# Input: 16 digits
-# Output: true or false
+# Input: 16 digits (integers)
+# Output: true or false (valid or invalid)
 # Steps: 
 	# Class CreditCard
 		# initialize
-			# create class: initialize to accept 16 digits
-				# IF input is not equal to 16
+			# set instance variable: accept 16 digits(integers) and convert to string
+				# IF digits are not equal to 16
 					# RAISE argument error
+				# ELSE split instance variable and insert space
 		# def double every other digit
-			# iterate over every other number 
-				# starting with 2nd to last index
-				# double each element as directed above
-			# sum all digits and separate doubled digits
-				# (find and?) separate doubled digits
-				# add all digits
-			# check validation
-				# if sum above is a multiple of 10
-					# return true or false (valid or invalid)
-
-# Initial Solution
+			# iterate over every other digit 
+				# separate even indexes and convert back to integers
+				# double each element (multiply by 2)
+					# untouched integers should just convert to integers?	
+			# return doubles variable and split characters of doubled digits
+				# join or add individual characters(of integers)
+				# iterate back to one total digit
+		# def checkcard
+			# set total equal to 0 (counter so it will count from 0)
+			# make sure digits are even
+			# join? add? to get total
+		# def validation
+				# if total above is a multiple of 10
+					# return valid or invalid (true or false)
 
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
 
+# Initial Solution 1 - 2 errors!!!
+
+# class CreditCard
+# 	def initialize(digits)
+
+# 		if @digits != 16
+# 			raise ArgumentError.new("Error: please enter 16 digits")
+# 		else @digits = digits
+# 		end
+# 	end
+# 	def check_card
+# 		def doubles
+# 			digits.each.at(-2).step(2) { |integer| p integer + integer }
+# 		end
+
+# 		def separate_then_total
+# 			total = 0
+# 			separated_nums = digits.to_s.split('').map! { |integertwo| integertwo.to_i }
+# 			separated_nums.each { |int| total += int}
+# 		end
+
+# 		def validate
+# 			total % 10 == 0
+# 		end
+# 	end
+# end
+
+
+# Initial Solution 2 - start over fresh!
+
+# class CreditCard
+# 	def initialize(digits)
+# 		@digitarray = digits
+    
+# 		@digitarray = digits.to_s.split('')
+# 		@digitarray.map!{ |value| value.to_i }
+
+#   		if @digitarray.length != 16
+#     		raise ArgumentError.new("Error: please enter 16 digits")
+#   		end
+# 	end
+
+# 	def split_digits_to_array
+# 		@digitarray = @digitarray.to_s.split("").map!{|x| x.to_i}
+# 	end
+
+# 	def doubles
+# 		@digitarray.map!.with_index do |integer, index|
+		
+# 		if index.even?
+# 			integer * 2
+# 		else
+# 			integer
+#      	end
+#     end
+
+#      @digitarray = @digitarray.join.to_s.split('')
+#      @digitarray.map! { |value| value.to_i }
+
+#    end
+
+# 	def sum_digits
+# 		@total = 0
+
+#      	@digitarray.each { |integer| @total += integer}
+
+# 		return @total
+# 	end
+
+# 	def check_card
+# 		doubles
+# 		sum_digits
+# 		@total % 10 == 0? true : false
+# 	end
+# end
+
+# Refactored Solution
+
 class CreditCard
 	def initialize(digits)
-		@digits = digits
+		@digitarray = digits.to_s.split('')
+		@digitarray.map!{ |value| value.to_i }
 
-		raise ArgumentError.new("Error: please enter 16 digits") unless @digits == 16
+  		if @digitarray.length != 16
+    		raise ArgumentError.new("Error: please enter 16 digits")
+  		end
+	end
+
+	def split_digits_to_array
+		@digitarray = @digitarray.to_s.split("").map! { |x| x.to_i }
+	end
+
+	def double_digits
+		@digitarray.map!.with_index do |integer, index|
+		
+		if index.even?
+			integer * 2
+		else
+			integer
+     	end
+    end
+
+     @digitarray = @digitarray.join.to_s.split('').map! { |value| value.to_i }
+
+   end
+
+	def sum_digits
+		@total = 0
+
+     	@digitarray.each { |integer| @total += integer}
+			return @total
 	end
 
 	def check_card
-		@split = @digits.to_s.split("")
-		@digits.map! { |character| character.to_i }
-
-		@separated_nums = @digits.map!.at(-2).step(2) { |integer| p integer + integer }
-
-		@total = @separated_nums.each { |int| @total += int}
-		
+		double_digits
+		sum_digits
 		@total % 10 == 0? true : false
 	end
 end
 
-# test1 = CreditCard.new(1234567891011121)
-
-
-# Refactored Solution
-
-
-
-
-
-
-
 
 # Reflection
+
+# - What was the most difficult part of this challenge for you and your pair?
+	# My pair and I had a really hard time with this challenge, particularly with calling methods
+	# and knowing when and where to call them. We kept getting two errors...
+
+# - What new methods did you find to help you when you refactored?
+
+# - What concepts or learnings were you able to solidify in this challenge?
+
