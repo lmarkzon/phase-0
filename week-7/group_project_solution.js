7b15654b31875f173a161bd199ef0d10b894d214
Lauren Markzon <lmarkzon@gmail.com>
2015-09-28 20:55:27 -0500

    Start 7.8

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,12 @@
+// Person 1 work: Tests to User Stories DELETE BEFORE SENDING!!
+	//As a user I want to be able to take in a list of numbers, perform calcuations, and get a single number as my output.  This output can have decimal points.
+
+	//As a user, I want to be able to use a function called sum to find the sum of a list of numbers.
+
+	// As a user, I want to be able to use a function called mean to find the average of a list of numbers.
+
+	// As a user, I want to to use a function called median that finds the median of a list of numbers of any length.  This is the middle number or decimal of the list.
+
+// Person 2 work: User Stories to Pseudocode
+
+
