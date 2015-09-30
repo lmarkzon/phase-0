5c607b8abbbed9443fbfa4aeda375fc2ed7fcc88
Lauren Markzon <lmarkzon@gmail.com>
2015-09-30 10:46:20 -0500

    7.8 person 2 done

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -1,12 +1,16 @@
-// Person 1 work: Tests to User Stories DELETE BEFORE SENDING!!
-	//As a user I want to be able to take in a list of numbers, perform calcuations, and get a single number as my output.  This output can have decimal points.
+// Release 2 work: User Stories to Pseudocode
 
-	//As a user, I want to be able to use a function called sum to find the sum of a list of numbers.
-
-	// As a user, I want to be able to use a function called mean to find the average of a list of numbers.
-
-	// As a user, I want to to use a function called median that finds the median of a list of numbers of any length.  This is the middle number or decimal of the list.
-
-// Person 2 work: User Stories to Pseudocode
-
-
+// input: list of numbers in the form of an array
+// create variable to count how many numbers received as input (find the length of array)
+// find the MEDIAN of the array of numbers by:
+	// sort the list of numbers in the array from lowest to highest
+	// IF the length of the array (count of the numbers on the list) is odd
+		// return median, which is only the middle number
+	// ELSE the length of the array is an even
+		// return the 2 middle numbers, those are the medians (need to make sure you are taking the MIDDLE ones)
+// use a function called sum to find the sum of the list of numbers by:
+	// set variable (total sum) that adds together the array of numbers received an input
+		// return total sum results from above
+// find the average of the array of numbers by(MEAN):
+	// divide total sum variable above by the length of the array variable created above
+		// output: return results, which is a single number (that can have decimal points)
