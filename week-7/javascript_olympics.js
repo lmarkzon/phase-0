f49772eb9c4491fa24753f35ee3af9d8ee2fb2bf
Lauren Markzon <lmarkzon@gmail.com>
2015-10-02 00:22:00 -0500

    7.6

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -31,13 +31,25 @@ reverse("hey")
 
 
 // Release 3: 2,4,6,8
+function even(array) {
+var n = array.filter(function(value){
+      if (value % 2 == 0) {
+        return value;
+      }
 
+  });
+console.log(n)
+}
 
+// Driver Code:
+var numbers = [1,4,6,7,8,10]
+var x = even(numbers)
 
 
 // Release 4: "We built this city"
-
-
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 
 
