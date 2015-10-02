af5f61334159bf44cd5a9bb9457c77ef2d659131
Lauren Markzon <lmarkzon@gmail.com>
2015-10-02 16:47:41 -0500

    7.6 fix release 1

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -7,50 +7,46 @@
 
 // Release 1: Bulk Up
 
-var athletes = [
-					ath1 = {name: "Sarah Hughes", event_name: "Ladies Single"},
-  	    			ath2 = {name: "Sarah Smith", event_name: "Ladies Doubles"}
-				]
+var athletes = ["Sarah Hughes", "Ladies Single"]
 
-// function olympics(athletes) {
-// athletes.name = name;
-// athletes.event_name = event_name;					// Create a function that takes an array of athletes that INCLUDE an name and event property.
-athletes.win = (" won the ");
-
-console.log(ath1.name + athletes.win + ath1.event_name + "!");
-console.log(ath2.name + athletes.win + ath2.event_name + "!");
-// };
-
-// Release 2: Jumble your words
-function reverse(string) {
-console.log(string.split('').reverse().join(''));
+function olympics(athletes) {
+	athletes.push("win")
+if (athletes.indexOf("win") != -1 ) {
+	console.log(athletes[0] + " won the " + athletes[1] + "!");
 }
+};
+
+olympics(athletes)
+
+// // Release 2: Jumble your words
+// function reverse(string) {
+// console.log(string.split('').reverse().join(''));
+// }
 	
-////DRIVER CODE
-reverse("hey")
+// ////DRIVER CODE
+// reverse("hey")
 
 
-// Release 3: 2,4,6,8
-function even(array) {
-var n = array.filter(function(value){
-      if (value % 2 == 0) {
-        return value;
-      }
+// // Release 3: 2,4,6,8
+// function even(array) {
+// var n = array.filter(function(value){
+//       if (value % 2 == 0) {
+//         return value;
+//       }
 
-  });
-console.log(n)
-}
+//   });
+// console.log(n)
+// }
 
-// Driver Code:
-var numbers = [1,4,6,7,8,10]
-var x = even(numbers)
+// // Driver Code:
+// var numbers = [1,4,6,7,8,10]
+// var x = even(numbers)
 
 
 // Release 4: "We built this city"
-var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
-console.log(michaelPhelps.constructor === Athlete)
-console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
-
+// var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+// console.log(michaelPhelps.constructor === Athlete)
+// console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 
 // Release 5: Reflection
