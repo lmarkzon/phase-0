e9f8ce1b753cfddc8aec7ae68e84c63a958dd566
Lauren Markzon <lmarkzon@gmail.com>
2015-10-01 19:04:53 -0500

    7.6

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -8,50 +8,28 @@
 // Warm Up
 
 var athletes = [
-					{name: "Sarah Hughes", event_name: "Ladies Single"},
-  	    			{name: "Sarah Smith", event_name: "Ladies Single"}
+					ath1 = {name: "Sarah Hughes", event_name: "Ladies Single"},
+  	    			ath2 = {name: "Sarah Smith", event_name: "Ladies Doubles"}
 				]
 
-function olympics(athletes) {
-athletes.name = name;
-athletes.event_name = event_name;					// Create a function that takes an array of athletes that INCLUDE an name and event property.
+// function olympics(athletes) {
+// athletes.name = name;
+// athletes.event_name = event_name;					// Create a function that takes an array of athletes that INCLUDE an name and event property.
 athletes.win = (" won the ");
 
-console.log(athletes.name + athletes.win + athletes.event_name + "!");
-};
-
-
-// Working Solution: Irina
-
-// function olympics(athletes) {
-
-//  athletes.map(function(value){
-//    win = "Sarah Hughes won the Ladies Single!"
-//    value.win = win
-//   });
-//  console.log(win)
-// }
-
-// var athletes = [
-
-//   ath1 = {name: "Sarah Hughes", event_name: "Ladies Single"},
-
-//   ath2 = {name: "Sarah Smith", event_name: "Ladies Single"}
-// ]
-
-// var x = olympics(athletes)
-// console.log(athletes)
-
-// Bulk Up
-
-
+console.log(ath1.name + athletes.win + ath1.event_name + "!");
+console.log(ath2.name + athletes.win + ath2.event_name + "!");
+// };
 
 // Jumble your words
-// function reverse(string) {
-// console.log(string.split('').reverse().join(''));
-// }
+function reverse(string) {
+console.log(string.split('').reverse().join(''));
+}
+	
+////DRIVER CODE
+reverse("hey")
 
-// var x = reverse("hey")
+
 
 // 2,4,6,8
 
