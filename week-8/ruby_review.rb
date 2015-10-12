c4cef29b965984ade33ed39de41dbd708ecc6d03
Lauren Markzon <lmarkzon@gmail.com>
2015-10-11 20:25:42 -0500

    ruby review

--- week-8/ruby_review.rb
+++ week-8/ruby_review.rb
@@ -1,3 +1,17 @@
+# Release 1: Complete the Challenge
+
+	# FizzBuzz Super Edition
+	
+
+				# JS FizzBuzz
+				# for (var number = 1; number <= 100; number += 1)
+				# 	if ((number % 3 == 0) && (number % 5 == 0)) console.log("FizzBuzz");
+				#     elsif (number % 3 == 0) console.log("Fizz");
+				# 	elsif (number % 5 == 0) console.log("Buzz");
+				# 	else console.log(number);
+
+# Numbers to English Words
+
 # Release 2: Reflect
 # - What concepts did you review or learn in this challenge?
 	# ...
