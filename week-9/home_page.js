7ca1704b7a4ef790d82dd87b3d2e8cbb05dc1c67
Lauren Markzon <lmarkzon@gmail.com>
2015-10-13 17:20:10 -0500

    9.3.1

---
+++ week-9/home_page.js
@@ -0,0 +1,34 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+
+
+// Release 1:
+document.getElementById("release-0").className = "done";
+
+// Release 2:
+document.getElementById("release-1").style.display = "none";
+
+// Release 3:
+document.getElementsByTagName("h1")[0].innerHTML = "I completed release 2.";
+
+// Release 4:
+document.getElementById("release-3").style.backgroundColor = "#955251";
+
+// Release 5:
+document.getElementsByClassName("release-4")[0].style.fontSize = "2em";
+
+// Release 6:
+var temp = document.getElementById("hidden");
+document.body.appendChild(temp.content.cloneNode(true));
+
+// Release 7: Reflect
+// - What did you learn about the DOM?
+	// ...
+// - What are some useful methods to use to manipulate the DOM?
+	// ...
