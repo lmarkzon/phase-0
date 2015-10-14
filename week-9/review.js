b953e1d9be8831c709ae456920026863e9aba727
Lauren Markzon <lmarkzon@gmail.com>
2015-10-14 00:28:17 -0500

    9.2

--- week-9/review.js
+++ week-9/review.js
@@ -1,17 +1,61 @@
-// 
+// Release 1: User Stories  
+  // User has a grocery list
+  // User adds an item with a quantity to the list
+  // User removes an item from the list
+  // User updates quantities for items in your list
+  // User is able to print the list (pretty)
 
-// Release 1: Pseudocode
-// Input:
-// Output:
-// Steps:
-	// ...
+// Release 2: Pseudocode
+// Input: Empty object that represents the grocery list
+// Output: printed list updated if applicable
+// Steps: 
+	// create empty new grocery list
+	// define function that adds item/qty to the list
+	// define function that deletes an item/qty from the list
+	// define function to loop over list
+		// find the item then update it's qty
+	// define function to print grocery list to console
 
 
-// Release 2: Initial Solution
+// Release 3: Initial Solution
+var groceryList = {}
 
-// Release 3: Refactor
+var addItem = function (item, qty, list) {
+	list[item] = qty;
+}
 
-// Release 4: Reflect
+var removeItem = function (item, list) {
+  delete list[item];
+}
+
+var updateQuantity = function (item, qty, list) {
+  list[item] = qty;
+}
+
+var printList = function (list){
+  for (item in list) {
+  	console.log(item + ", " + list[item]);
+  }
+}
+
+// DRIVER CODE
+addItem(groceryList, "Lemonade", 1)
+addItem(groceryList, "Tomatoes", 2)
+addItem(groceryList, "Onions", 3)
+addItem(groceryList, "Ice Cream", 4)
+printList(groceryList)
+console.log(groceryList)
+// removeItem(groceryList)
+
+// updateQuantity(groceryList)
+
+
+
+// Release 4: Refactor
+
+
+
+// Release 5: Reflect
 // - What concepts did you solidify in working on this challenge? 
 	// ...
 // - What was the most difficult part of this challenge?
