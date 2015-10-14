92be4134e66c2d311c6b80fa068bb4a22d8b0809
Lauren Markzon <lmarkzon@gmail.com>
2015-10-14 10:00:57 -0500

    Work on 9.2 - not done

--- week-9/review.js
+++ week-9/review.js
@@ -17,47 +17,103 @@
 	// define function to print grocery list to console
 
 
-// Release 3: Initial Solution
-var groceryList = {}
+// Release 3: Initial Solution 1: basic ruby solution translated to JS, doesn't work right
+// var groceryList = {}
 
-var addItem = function (item, qty, list) {
-	list[item] = qty;
-}
+// var addItem = function (item, qty, list) {
+// 	list[item] = qty;
+// }
 
-var removeItem = function (item, list) {
-  delete list[item];
-}
+// var removeItem = function (item, list) {
+//   delete list[item];
+// }
 
-var updateQuantity = function (item, qty, list) {
-  list[item] = qty;
-}
+// var updateQuantity = function (item, qty, list) {
+//   list[item] = qty;
+// }
 
-var printList = function (list){
-  for (item in list) {
-  	console.log(item + ", " + list[item]);
-  }
-}
+// var printList = function (list){
+//   for (item in list) {
+//   	console.log(item + ", " + list[item]);
+//   }
+// }
 
-// DRIVER CODE
-addItem(groceryList, "Lemonade", 1)
-addItem(groceryList, "Tomatoes", 2)
-addItem(groceryList, "Onions", 3)
-addItem(groceryList, "Ice Cream", 4)
-printList(groceryList)
-console.log(groceryList)
+// // DRIVER CODE
+// addItem(groceryList, "Onions", 3)
+// printList(groceryList)
+// console.log(groceryList)
 // removeItem(groceryList)
-
 // updateQuantity(groceryList)
 
+// Initial Solution 2: 
+var groceryList = [];
+
+function addItem(item){
+    groceryList.push(item);
+}
+
+function removeItem(item){
+    for (var i = 0; i <= groceryList.length; i++){
+        if (groceryList[i] === item) groceryList.splice(i, 1);
+    }
+}
+
+function printList(){
+    for (var i = 0; i < groceryList.length; i++){
+        if (groceryList.length == 0)
+        return;
+        else
+        console.log("- " + groceryList[i]);
+    	}
+	}
+}
+
+printList();
+addItem('Onions');
 
 
-// Release 4: Refactor
+
+// Release 4: Refactored using constructors/prototypes :)
+// function GroceryList() {
+// }
+
+// GroceryList.prototype.addItem = function addItem(list, item, qty) {
+//   list[item] = qty;
+// }
+
+// GroceryList.prototype.removeItem = function removeItem(list, item) {
+//   delete list[item];
+// }
+
+// GroceryList.prototype.updateQuantity = function updateQuantity(list, item, quantity) {
+//   list[item] = quantity;
+// }
+
+
+// function newList() {
+//   this.addItem = function(list, item, qty) {
+//     list[item] = qty;
+//   }
+
+//   this.removeItem = function(list, item) {
+//     delete list[item];
+//   }
+
+//   this.updateQuantity = function(list, item, quantity) {
+//     list[item] = quantity;
+//   }
+// }
+
+// // DRIVER CODE
+// var newList = new GroceryList();
+// newList.addItem(newList, "Apples", 1)
+// console.log(newList)
 
 
 
 // Release 5: Reflect
 // - What concepts did you solidify in working on this challenge? 
-	// ...
+	// I really enjoyed this challenge, I had never worked with 
 // - What was the most difficult part of this challenge?
 	// ...
 // - Did you solve the problem in a new way this time?
