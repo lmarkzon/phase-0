1bfc140da0fedf51ed259b56b5be38e1722aa51a
Lauren Markzon <lmarkzon@gmail.com>
2015-10-18 20:49:50 -0500

    Ruby review 2 9.5

--- week-9/ruby-review-2/ruby-review.rb
+++ week-9/ruby-review-2/ruby-review.rb
@@ -1,11 +1,8 @@
 # Release 1: Complete the Challenge
 # U2.W6: Testing Assert Statements
-
 # I worked on this challenge [by myself].
 
-
 # 2. Review the simple assert statement
-
 def assert
   raise "Assertion failed!" unless yield
 end
@@ -20,56 +17,46 @@ assert { name == "bettysue" }
 	# (You invoke a block by using the yield statement)
 # will pass as long as your test code is correct (in this case bettysue)
 
-
 # 3. Copy your selected challenge here
-class CreditCard
-	def initialize(digits)
-		@digitarray = digits.to_s.split('').map!{ |value| value.to_i }
+class Die
+  def initialize(sides)
+    if sides < 1
+    	raise ArgumentError.new("Error: Less than 1 size")
+    else @side_nums = sides
+    end
+  end
 
-  		if @digitarray.length != 16
-    		raise ArgumentError.new("Error: please enter 16 digits")
-  		end
-	end
+  def sides
+    "Number of sides: #{@side_nums}"
+  end
 
-	def split_digits_to_array
-		@digitarray = @digitarray.to_s.split("").map! { |x| x.to_i }
-	end
-
-	def double_digits
-		@digitarray.map!.with_index do |integer, index|
-		
-		if index.even?
-			integer * 2
-		else
-			integer
-     		end
-    	end
-   	end
-
-	def sum_digits
-		@digitarray = @digitarray.join.to_s.split('').map! { |value| value.to_i }
-
-		@total = 0
-
-     	@digitarray.each { |integer| @total += integer}
-			return @total
-	end
-
-	def check_card
-		double_digits
-		sum_digits
-		
-		@total % 10 == 0 ? true : false
-	end
+  def roll
+    @roll = rand(1..@side_nums)
+    "Number rolled: #{@roll}"
+  end
 end
 
-
-CreditCard.new(11111111111111112)
+# DRIVER CODE
+test = Die.new(6)
+p test.sides
+p test.roll
 
 # 4. Convert your driver test code from that challenge into Assert Statements
-
-
-
-
+assert {test = Die.new(6)}
+assert {test.sides}
+assert {test.roll}
 
 # 5. Reflection
+# - What concepts did you review in this challenge?
+	# It was nice to just have a general review of Ruby in this challenge, specifically
+	# more review and practice with Ruby classes, and now with assertions as well. 
+# - What is still confusing to you about Ruby?
+	# I'm still a bit confused with the difference between ruby and rspec, but I'm going
+	# to research this to help with my confusion. I also need to just get more practice 
+	# working with Ruby classes, since they intimidate me quite a bit. I'm also still a
+	# little confused on what exactly the purpose of assertions are, but I also plan to 
+	# research this a bit more.
+# - What are you going to study to get more prepared for Phase 1?
+	# I'm going to be studying and reviewing a lot of Ruby, I feel like learning JavaScript
+	# recently has wiped away everything I learned about Ruby. So really just reviewing key'
+	# Ruby concepts, terms, and practice with defining methods and using classes.
